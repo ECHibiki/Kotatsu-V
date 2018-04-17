2c2a920bffba3656ca24c898f7c810cf8e7339bf
4taba <4taba@protonmail.com>
2018-04-17 00:25:40 -0600

    settings

--- default_settings.py
+++ default_settings.py
@@ -12,6 +12,9 @@ FFpath = 'ffmpeg' # Set this to the ffmpeg command for creating thumbnails. Feel
 Allow_Email = False # Allow users to post any text (including email addresses) in the Email form field
 TimeoutThread = 120 # Amount of seconds users must wait between creating threads
 TimeoutPost = 15 # Amount of seconds users must wait between posts
+MaxPostLength = 8000 # Maximum character count per post
+MaxPostLines = 200 # Maximum line count per post
+PostLineCutoff = 30 # Lines to display on board views (where the post is cut giving a message "view entire thread to see full comment")
 BannerRandom = False # Should banner rotation pick a random banner or simply cycle through them in order?
 BannerRotationTime = 5 # In minutes (This is only used if BannerRandom=False. When banners are random they will change every page load rather than using a rotation time)
 Banners = ( 'banner1.jpg', 'banner2.jpg', 'banner3.jpg' ) # List of images inside res/banners to use for banners
@@ -43,25 +46,25 @@ DBPASS = ''
 # POSTER-UP - set upload filetypes allowed by normal (non opening) posts
 BoardInfo = {
     # Listed boards
-    #KEY   ( NAME             STYLE       USERNAME     THREADS POSTS  DISPLAY   OP-UP      POSTER-UP )
-    'ni':  ('日本裏'  ,      'mona',     '名無しさん', 150,    200,  'normal', 'img+vid', 'img+vid'),
-    'd':   ('二次元エロ',    'yotsuba',  'Anonymous',  150,    200,  'normal', 'img+vid', 'img+vid'),
-    'cc':  ('Computer Club', 'computer', 'guest@cc',   150,    200,  'normal', 'img+vid', 'img+vid'),
-    'f':   ('Flash/HTML5',   'yotsuba',  'Anonymous',  30,     200,  'flash',  'flash',   ''       ),
-    'ho':  ('Other',         'yotsuba',  'Anonymous',  150,    200,  'normal', 'img+vid', 'img+vid'),
+    #KEY   ( NAME             STYLE       USERNAME     THREADS POSTS  DISPLAY   OP-UP      POSTER-UP  LISTED )
+    'ni':  ('日本裏'  ,      'mona',     '名無しさん', 150,    200,  'normal', 'img+vid', 'img+vid',  True   ),
+    'd':   ('二次元エロ',    'yotsuba',  'Anonymous',  150,    200,  'normal', 'img+vid', 'img+vid',  True   ),
+    'cc':  ('Computer Club', 'computer', 'guest@cc',   150,    200,  'normal', 'img+vid', 'img+vid',  True   ),
+    'f':   ('Flash/HTML5',   'yotsuba',  'Anonymous',  30,     200,  'flash',  'flash',   ''       ,  True   ),
+    'ho':  ('Other',         'yotsuba',  'Anonymous',  150,    200,  'normal', 'img+vid', 'img+vid',  True   ),
 
     # Unlisted boards
-    #KEY    ( NAME               STYLE      USERNAME    THREADS POSTS  DISPLAY   OP-UP            POSTER-UP )
-    '*':    ('Unlisted',        'unlisted', 'Anonymous', 150,    200, 'normal', 'img+vid',       'img+vid'      ),
-    'meta': ('Meta Discussion', 'unlisted', 'Anonymous', 150,    200, 'normal', 'img+vid+flash', 'img+vid+flash'),
+    #KEY    ( NAME               STYLE      USERNAME    THREADS POSTS  DISPLAY   OP-UP            POSTER-UP       LISTED )
+    '*':    ('Unlisted',        'unlisted', 'Anonymous', 150,    200, 'normal', 'img+vid',       'img+vid',       False  ),
+    'meta': ('Meta Discussion', 'unlisted', 'Anonymous', 150,    200, 'normal', 'img+vid+flash', 'img+vid+flash', False  ),
 
     ### Avoid modifying these for now
     ### These are special boards. Modifying them should be fairly easy still,
     ### but will require some changes inside the server code, otherwise something might break.
-    #KEY        ( NAME                   STYLE       USERNAME THREADS POSTS  DISPLAY   OP-UP  POSTER-UP )
-    'listed':   ('All Listed Boards',   'main',     '',       0,      0,    'normal', '',    ''),
-    'unlisted': ('All Unlisted Boards', 'unlisted', '',       1500,   0,    'normal', '',    ''), 
-    'all':      ('All Boards',          'main',     '',       0,      0,    'normal', '',    ''),
+    #KEY        ( NAME                   STYLE       USERNAME THREADS POSTS  DISPLAY   OP-UP  POSTER-UP  LISTED )
+    'listed':   ('All Listed Boards',   'main',     '',       -1,     0,    'normal', '',    '',         True   ),
+    'unlisted': ('All Unlisted Boards', 'unlisted', '',       1500,   0,    'normal', '',    '',         True   ), 
+    'all':      ('All Boards',          'main',     '',       -1,     0,    'normal', '',    '',         True   ),
 }
 
 # List of disallowed board names. In addition to this list any boards containing quotes or beginning with a period are also disallowed for security reasons
@@ -88,47 +91,48 @@ StyleTransparencies = {
 Filters = [
             # === Post link filters === (Order is important here)
                 ('cross-board-cross-thread-post-link',
-                 re.compile(r'[^">]&gt;&gt;&gt;/([^/ ]*)/([0-9]+)/([0-9]+)'),
-                 '<a href="/\1/\2#\3">&gt;&gt;&gt;/\1/\2/\3</a>'),
+                 re.compile(r'(?<!&gt;)&gt;&gt;&gt;/([^/ ]*)/([0-9]+)/([0-9]+)'),
+                 r'<a href="/\1/\2#\3">&gt;&gt;&gt;/\1/\2/\3</a>'),
 
                 ('cross-board-cross-thread-link',
                  re.compile('[^">]&gt;&gt;&gt;/([^/ ]*)/([0-9]+)'),
-                 '<a href="/\1/\2">&gt;&gt;&gt;/\1/\2</a>'),
+                 r'<a href="/\1/\2">&gt;&gt;&gt;/\1/\2</a>'),
 
                 ('cross-board-link',
                  re.compile('[^">]&gt;&gt;&gt;/([^/ ]*)/'),
-                 '<a href="/\1">&gt;&gt;&gt;/\1/</a>'),
+                 r'<a href="/\1">&gt;&gt;&gt;/\1/</a>'),
 
                 ('cross-thread-link',
                  re.compile(r'[^">]&gt;&gt;&gt;([0-9]+)/([0-9]+)'),
-                 '<a href="/%s/\1#\2">&gt;&gt;&gt;/%s/\1/\2</a>'),
+                 r'<a href="/%s/\1#\2">&gt;&gt;&gt;/%s/\1/\2</a>'),
                  #NOTE: This is not necessarily the most elegant solution
                  #      But for now, there is an if-statement in the server code that will look for the label: cross-thread-link
                  #      and fill in the %s's here with the board name
 
                 ('post-link',
-                 re.compile(r'[^">]&gt;&gt;([0-9]+)'),
-                 '<a href="/%s/%s#\1">&gt;&gt;\1</a>'),
+                 re.compile(r'(?<!&gt;)&gt;&gt;([0-9]+)'),
+                 r'<a href="/%s/%s#\1">&gt;&gt;\1</a>'),
                  #NOTE: Same here
             # =========================
 
             ('quote',
-             re.compile(r'(^&gt;[^<]*)'),
-             '<span class="quote">\1</span>'),
+             #re.compile(r'(^&gt;[^<]*)'),
+             re.compile(r'(^|<br>)(&gt;[^<]*)'),
+             r'\1<span class="quote">\2</span>'),
 
             ('url',
-             re.compile(r'(^|>)(&gt;[^<]*)'),
-             '<a href="\1\2">\1\2</a>\3'),
+             re.compile(r'([^ >]*)://([^ <]*)'),
+             r'<a href="\1://\2">\1://\2</a>'),
 
-            ('code',
-             re.compile(r'\[code\](.*)\[/code\]'),
-             '<span class="code">\1</span>'),
+#            ('code',
+#             re.compile(r'\[code\](.*)\[/code\]'),
+#             r'<span class="code">\1</span>'),
 
-            ('japanese',
-             re.compile(r'\[ja\](.*)\[/ja\]'),
-             '<span class="ja">\1</span>'),
+#            ('japanese',
+#             re.compile(r'\[ja\](.*)\[/ja\]'),
+#             r'<span class="ja">\1</span>'),
 
-            ('spoiler',
-             re.compile(r'\[spoiler\](.*)\[/spoiler\]'),
-             '<span class="spoiler">\1</span>'),
+#            ('spoiler',
+#             re.compile(r'\[spoiler\](.*)\[/spoiler\]'),
+#             r'<span class="spoiler">\1</span>'),
 ]
