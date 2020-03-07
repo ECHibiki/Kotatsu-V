e4571ced0953ddd509505e29bc46113e5d5241ad
ECHibiki <ecverniy@gmail.com>
2020-03-07 03:28:49 -0500

    Make unit test a little more usefull

--- test/unit/Test_BB_Tag_Propery_Outputs_Divisions.scm
+++ test/unit/Test_BB_Tag_Propery_Outputs_Divisions.scm
@@ -8,6 +8,16 @@
 (newline)(newline)
 (newline)(newline)
 
+(display "Must Pass Code tests")(newline)
 (display (string=? (reluctant-code-tags "a [code]1[/code] [code]2[/code] b") "a <div class=''code''>1</div> <div class=''code''>2</div> b"))
 (newline)(newline)
-(display (string=? (reluctant-code-tags "a [codeblock][code]1[/code][/codeblock] b [code]2[/code] c")))
+(display (string=? (reluctant-code-tags "a [codeblock][code]1[/code][/codeblock] b [code]2[/code] c") "a <div class=''code''>[code]1[/code]</div> b <div class=''code''>2</div> c"))
+
+(newline)(newline)
+
+(display "Must Fail Code tests")(newline)
+(display (string=? (reluctant-code-tags "a [code]1[/code] [code]2[/code] b") "a <div class=''code''>1[/code] [code]2</div> b"))
+(newline)(newline)
+(display (string=? (reluctant-code-tags "a [codeblock][code]1[/code][/codeblock] b [code]2[/code] c") "a <div class=''code''>1[/code][/codeblock] b [code]2</div> c"))
+(newline)(newline)
+
